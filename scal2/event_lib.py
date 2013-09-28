63ecb3b691f3f423178fb7bada62911007f867db
Saeed Rasooli <saeed.gnu@gmail.com>
2013-09-28 21:52:35 +0330

    fix in event_lib: info.load() when json file is not created yet

--- scal2/event_lib.py
+++ scal2/event_lib.py
@@ -93,7 +93,10 @@ class InfoWrapper(JsonSObjBase):
         self.save()
 
 info = InfoWrapper()
-info.load()
+try:
+    info.load()
+except IOError:
+    pass
 
 ###################################################
 
